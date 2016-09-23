cf0c16dc9af37de0dc9caf6748b1376eb26c3062
dafi@nodes.dk <dafi@nodes.dk>
2016-09-23 14:53:53 +0200

    custom: get develop branch up to date

--- docs/example.js
+++ docs/example.js
@@ -9,10 +9,46 @@
 		.module('demoApp')
 		.controller('demoController', function() {
 			var vm = this;
-			vm.testValue = 'Greetings from Angular with custom filter!';
-			vm.controllerMethod = function(message) {
-				console.log(message);
+			
+			vm.nodesCDNoptions = {
+				loadingTemplateUrl: 'loading-test.html'
 			};
+			
+			vm.imageOptions = {
+				// fallbackImage: 'http://placehold.it/768x100/000000/ffffff&text=fallbackImage',
+				loadingTemplateUrl: 'loading-test.html',
+				// loadingTemplate: '<h1>Nedlaster...</h1>',
+				disableLazy: false,
+				nodesCDN: false,
+				aspectRatio: '4-3'
+			};
+			
+			vm.altText = 'My alt text defined in some controller';
+			
+			vm.images = [];
+			vm.innerImages = [];
+			
+			vm.randomize = function() {
+				vm.images = [];
+				for(var i = 0; i < 100; i++) {
+					var img = _generateRandomImage();
+					vm.images.push(img);
+				}
+			};
+			vm.randomizeInner = function() {
+				vm.innerImages = [];
+				for(var i = 0; i < 100; i++) {
+					var img = _generateRandomImage();
+					vm.innerImages.push(img);
+				}
+			};
+			vm.randomize();
+			vm.randomizeInner();
+			
+			function _generateRandomImage() {
+				var color = Math.floor(Math.random()*16777215).toString(16);
+				return 'http://placehold.it/800x600/' + color + '/ffffff';
+			}
 		});
 	
 })();
